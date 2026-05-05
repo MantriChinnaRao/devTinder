1b4f9661f74e313864832036392b88246b6d4e7f
MantriChinnaRao <153576655+MantriChinnaRao@users.noreply.github.com>
2026-05-05 15:23:27 +0530

    get post delete

--- src/app.js
+++ src/app.js
@@ -2,8 +2,32 @@ const express=require("express");
 
 const app=express();
 
- app.use("/",(req,res)=>{
-    res.send("bokka laga")
+
+  app.use("/hello/2",(req,res)=>{
+    res.send("edhi rendo edhi ra thammudu")
+  })
+
+    app.use("/hello",(req,res)=>{
+    res.send("hello people")
+  })
+
+  app.get("/user",(req,res)=>{
+      
+    res.send({
+      "firstname":"chinnarao",
+      "lastname":"mantri"
+    })
+      
+  })
+
+
+  app.post("/user",(req,res)=>{
+     res.send("successfully deployed post")
+  })
+     
+  //this will match all the http method Api calls to
+   app.use("/user",(req,res)=>{
+     res.send("hahahahhhhahahah")
   })
 
   app.use("/testt",(req,res)=>{
@@ -13,9 +37,6 @@ const app=express();
   app.use("/live",(req,res)=>{
     res.send("live your application");
   })
-
-
-
 app.listen(7777,()=>{
     console.log("server is successfully listening on port 7777....");
 });
